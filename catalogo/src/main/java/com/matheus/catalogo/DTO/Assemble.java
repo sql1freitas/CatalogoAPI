850de823d93168e4483aec177816c83085e90392
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2023-12-25 23:30:28 -0300

    3 commit

--- catalogo/src/main/java/com/matheus/catalogo/DTO/Assemble.java
+++ catalogo/src/main/java/com/matheus/catalogo/DTO/Assemble.java
@@ -9,13 +9,7 @@ import org.springframework.stereotype.Component;
 public class Assemble {
     public ItensDTO itensToDTO(Itens itens){
 
-          ItensDTO itensDTO = new ItensDTO();
-          itensDTO.setId(itens.getId());
-          itensDTO.setTitle(itens.getTitle());
-          itensDTO.setImage(itens.getImage());
-          itensDTO.setValue(itens.getValue());
 
-
-        return itensDTO;
+        return new ItensDTO(itens.getId(), itens.getTitle(),itens.getImage(), itens.getValue());
     }
 }
--- catalogo/src/main/java/com/matheus/catalogo/DTO/ItensDTO.java
+++ catalogo/src/main/java/com/matheus/catalogo/DTO/ItensDTO.java
@@ -9,19 +9,39 @@ public class ItensDTO {
     private String image;
     private Double value;
 
+    public ItensDTO(Long id, String title, String image, Double value) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.value = value;
+    }
 
-
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getImage() {
+        return image;
+    }
+
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public Double getValue() {
+        return value;
     }
 
     public void setValue(Double value) {
